903c899c9596f849917189cb9459c474f9178fe6
yutaka.miyazaki <mono.12dev@gmail.com>
2019-12-19 00:36:05 +0900

    initial commit

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+// ref: [https://storybook.js.org/docs/configurations/typescript-config/]
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("awesome-typescript-loader")
+      },
+      // Optional
+      {
+        loader: require.resolve("react-docgen-typescript-loader")
+      }
+    ]
+  });
+  config.resolve.extensions.push(".ts", ".tsx", "js");
+  return config;
+};
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// ref: [https://storybook.js.org/docs/configurations/typescript-config/]
+
+module.exports = {
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/lib/"],
+  testRegex: "(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "json"]
+};
